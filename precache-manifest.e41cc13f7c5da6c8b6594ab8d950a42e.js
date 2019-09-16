b325d5741dae1748f648fe348593778c91c83c94
Yaroslav <root@MacBook-Pro-Aroslav.local>
2019-09-17 00:56:52 +0400

    Updates

---
+++ precache-manifest.e41cc13f7c5da6c8b6594ab8d950a42e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6b3b1c46935758db2d57722b53e0f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46ea589164dd4ea18a6b",
+    "url": "./static/css/2.a83908c5.chunk.css"
+  },
+  {
+    "revision": "bea94481a6269052dffb",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "46ea589164dd4ea18a6b",
+    "url": "./static/js/2.c8a5c99f.chunk.js"
+  },
+  {
+    "revision": "bea94481a6269052dffb",
+    "url": "./static/js/main.86a994ea.chunk.js"
+  },
+  {
+    "revision": "7f6e2644a9fa3096419c",
+    "url": "./static/js/runtime~main.e69e93f5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
